9cd341b4b1cce0df1bf176c9c82f93fb490b5cd4
betchi <shinichi.minobe@gmail.com>
2018-05-25 01:20:46 +0900

    update

--- src/const.ts
+++ src/const.ts
@@ -19,6 +19,8 @@ export enum MessageType {
   TEXT = 'text',
   IMAGE = 'image',
   FILE = 'file',
+  INDICATOR = 'indicator',
+  UPDATE_ROOM_USER = 'updateRoomUser',
 }
 
 export enum MessageActionType {
